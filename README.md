fd68c93f8a2a42e10c5b52525429cfa681127a15
Çağlar YILDIRIM <cglr.1998@hotmail.com>
2024-09-16 09:01:55 +0300

    general changes

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Native Animations 
+
+Sample animation studies using React-native 
+
+## 1. Modal Animation
+
+![a](./assets/gifs/modal_animation.gif)
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Link, Stack } from "expo-router";
 
 const HomeRouter = () => {
 	return (
-		<View>
+		<View style={styles.container}>
 			<Stack.Screen
 				options={{
 					headerTitle: "Home",
@@ -14,5 +14,14 @@ const HomeRouter = () => {
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
 
 export default HomeRouter;
