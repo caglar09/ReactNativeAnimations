b409c68bf67298db85b76744378fcb9486e82208
Çağlar YILDIRIM <cglr.1998@hotmail.com>
2024-09-22 20:11:17 +0300

    Changed Easing function

--- components/TickerText.tsx
+++ components/TickerText.tsx
@@ -43,10 +43,20 @@ const TickerList = ({ fontSize, number, index }: TickerListProps) => {
 		animatedValue.value = withDelay(
 			index * stagger,
 			withTiming(number, {
-				easing: Easing.bezier(0.68, -0.6, 0.32, 1.6),
+				easing: (x: number) => {
+					"worklet";
+					const c1 = 1.70158;
+					const c2 = c1 * 1.525;
+
+					return x < 0.5
+						? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+						: (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+				},
 				reduceMotion: ReduceMotion.System,
 			})
 		);
+
+		// Easing.bezierFn()
 
 		return () => {
 			cancelAnimation(animatedValue);
